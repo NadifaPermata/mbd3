d59de06191e7b0c9cfac2e28c374f1d879c58a4a
NadifaPermata <129677228+NadifaPermata@users.noreply.github.com>
2023-11-29 14:58:13 +0000

    Co-authored-by: rantianindaa <rantianindaa@users.noreply.github.com>

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ list_symptom = ['', 'pertalite', 'pertamax', 'pertamax turbo', 'solar', 'pertami
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://NadifaPermata:7CIXwskWNRy0@ep-falling-cherry-06864175.us-east-2.aws.neon.tech/web")
 with conn.session as session:
-    query = text('CREATE TABLE IF NOT EXISTS SELLING (id serial, nama_petugas varchar, plat_nomor char(25), jenis_kendaraan varchar, \
+    query = text('CREATE TABLE IF NOT EXISTS selling (id serial, nama_petugas varchar, plat_nomor char(25), jenis_kendaraan varchar, \
                                                        symptom text, banyak_pembelian varchar, tanggal date);')
     session.execute(query)
 
@@ -22,7 +22,7 @@ if page == "Edit Data":
     if st.button('Tambah Data'):
         with conn.session as session:
             query = text('INSERT INTO selling (nama_petugas, plat_nomor, jenis_kendaraan, symptom, banyak_pembelian, waktu, tanggal) \
-                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
+                          VALUES (:1, :2, :3, :4, :5, :6, :7 :8);')
             session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':None, '8':None})
             session.commit()
 
@@ -31,20 +31,19 @@ if page == "Edit Data":
         id = result['id']
         nama_petugas_lama = result["nama_petugas"]
         plat_nomor_lama = result["plat_nomor"]
+        jenis_kendaraan_lama = result["jenis_kendaraan"]
         symptom_lama = result["symptom"]
-        banyak_pembelianlama = result["handphone"]
-        address_lama = result["address"]
+        banyak_pembelian_lama = result["banyak_pembelian"]
         waktu_lama = result["waktu"]
         tanggal_lama = result["tanggal"]
 
-        with st.expander(f'a.n. {patient_name_lama}'):
+        with st.expander(f'{plat_nomor_lama}'):
             with st.form(f'data-{id}'):
-                doctor_name_baru = st.selectbox("doctor_name", list_doctor, list_doctor.index(doctor_name_lama))
-                patient_name_baru = st.text_input("patient_name", patient_name_lama)
-                gender_baru = st.selectbox("gender", list_symptom, list_symptom.index(gender_lama))
-                symptom_baru = st.multiselect("symptom", ['cough', 'flu', 'headache', 'stomache'], eval(symptom_lama))
-                handphone_baru = st.text_input("handphone", handphone_lama)
-                address_baru = st.text_input("address", address_lama)
+                nama_petugas_baru = st.selectbox("nama_petugas", list_petugas, list_petugas.index(nama_petugas_lama))
+                plat_nomor_baru = st.text_input("plat_nomor", plat_nomor_lama)
+                jenis_kendaraan_baru = st.text_input("jenis_kendaraan", jenis_kendaraan_lama)
+                symptom_baru = st.multiselect("symptom", ['pertalite', 'pertamax', 'pertamax turbo', 'solar', 'pertamina dex'], eval(symptom_lama))
+                banyak_pembelian_baru = st.text_input("banyak_pembelian", banyak_pembelian_lama)
                 waktu_baru = st.time_input("waktu", waktu_lama)
                 tanggal_baru = st.date_input("tanggal", tanggal_lama)
                 
@@ -53,18 +52,18 @@ if page == "Edit Data":
                 with col1:
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
-                            query = text('UPDATE schedule \
-                                          SET doctor_name=:1, patient_name=:2, gender=:3, symptom=:4, \
-                                          handphone=:5, address=:6, waktu=:7, tanggal=:8 \
-                                          WHERE id=:9;')
-                            session.execute(query, {'1':doctor_name_baru, '2':patient_name_baru, '3':gender_baru, '4':str(symptom_baru), 
-                                                    '5':handphone_baru, '6':address_baru, '7':waktu_baru, '8':tanggal_baru, '9':id})
+                            query = text('UPDATE selling \
+                                          SET nama_petugas=:1, plat_nomor=:2, symptom=:3, jenis_kendaraan=:4 \
+                                          banyak_pembelian=:5, waktu=:6, tanggal=:7 \
+                                          WHERE id=:7;')
+                            session.execute(query, {'1':nama_petugas, '2':plat_nomor_baru, '3':str(symptom_baru), '4':jenis_kendaraan, 
+                            '5':banyak_pembelian_baru, '6':waktu_baru, '7':tanggal_baru, '8':id})
                             session.commit()
                             st.experimental_rerun()
                 
                 with col2:
                     if st.form_submit_button('DELETE'):
-                        query = text(f'DELETE FROM schedule WHERE id=:1;')
+                        query = text(f'DELETE FROM selling WHERE id=:1;')
                         session.execute(query, {'1':id})
                         session.commit()
                         st.experimental_rerun()
